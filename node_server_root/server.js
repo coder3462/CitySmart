90a60596dd95b2d8aa672ea56381d6dc3ce39a27
Varun Ramani <varun.ramani@gmail.com>
2018-02-24 22:44:11 -0500

    I restructured the project

--- node_server_root/server.js
+++ node_server_root/server.js
@@ -4,12 +4,16 @@ const path = require('path');
 
 // Constants that are not imports
 const app = express();
-// const dirname = path.resolve(path.resolve(), '../') + "/";
+const dirname = path.resolve(path.resolve('../frontend'));
+
 
 // console.log(dirname)
 
 var router = express.Router();
 
-app.get('/', (req, res) => res.sendFile("/home/varun/Documents/Software/Hackathons/ProfHacks/profhacks-2018/frontend/html/main.html"));
+// app.get('/', (req, res) => res.sendFile("/home/varun/Documents/Software/Hackathons/ProfHacks/profhacks-2018/frontend/html/main.html"));
+
+app.get('/', (req, res) => res.sendFile(`${dirname}/index.html`));
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
