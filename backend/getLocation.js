c40789acdc86bbf2636ee548cc33c5d452e237b6
Samvit123 <samvitagarwal03@gmail.com>
2018-02-24 19:07:00 -0500

    Create getLocation.js

---
+++ backend/getLocation.js
@@ -0,0 +1,21 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  var lat = crd.latitude;
+  var long = crd.longitude;
+  
+  console.log(lat+","+long);
+
+  
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
